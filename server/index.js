66509e4e9fee318c3dad711c1ad96a7a4bae4fd2
Gian Franco Lazaro <gianlazaro7411@gmail.com>
2022-07-27 10:25:46 -0700

    Schema changes

--- server/index.js
+++ server/index.js
@@ -9,7 +9,7 @@ app.use(express.json());
 app.get('/reviews', (req, res) => {
   const { page, count, sort, product_id } = req.query;
 
-  pool.query(`SELECT json_build_object('product', $1::integer, 'page', $2::integer, 'count', $3::integer, 'results', json_agg(json_build_object('review_id', r.id, 'rating', r.rating, 'summary', r.summary, 'recommend', r.recommend, 'response', r.response, 'body', r.body, 'date', to_timestamp(r.date/1000), 'reviewer_name', r.reviewer_name, 'helpfulness', r.helpfulness, 'photos', json_build_array(rp)) )) FROM reviews r INNER JOIN reviews_photos rp ON rp.review_id = r.id WHERE product_id = $1`, [product_id, page, count], (err, results) => {
+  pool.query(`SELECT json_build_object('product', $1::integer, 'page', $2::integer, 'count', $3::integer, 'results', json_agg(json_build_object('review_id', r.id, 'rating', r.rating, 'summary', r.summary, 'recommend', r.recommend, 'response', r.response, 'body', r.body, 'date', to_timestamp(r.date/1000), 'reviewer_name', r.reviewer_name, 'helpfulness', r.helpfulness, 'photos', json_build_array(rp)))) FROM reviews r INNER JOIN reviews_photos rp ON rp.review_id = r.id WHERE product_id = $1`, [product_id, page, count], (err, results) => {
     if (err) throw err;
     res.send(results.rows[0]);
   });
