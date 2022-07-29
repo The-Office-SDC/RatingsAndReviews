93df7de418a1dadb93a22c80d7bde35e3686eaa7
Gian Franco Lazaro <gianlazaro7411@gmail.com>
2022-07-28 20:18:42 -0700

    querying for most routes almost done

--- schema.sql
+++ schema.sql
@@ -6,40 +6,47 @@ CREATE DATABASE rr;
 \c rr;
 
 CREATE TABLE reviews (
-  id              integer NOT NULL PRIMARY KEY,
+  id              serial NOT NULL PRIMARY KEY,
   product_id      integer NOT NULL,
   rating          smallint NOT NULL,
-  date            bigint NOT NULL,
+  date            text NOT NULL,
   summary         text NOT NULL,
   body            text NOT NULL,
-  recommend       boolean NOT NULL,
-  reported        boolean NOT NULL,
+  recommend       boolean NOT NULL DEFAULT false,
+  reported        boolean NOT NULL DEFAULT false,
   reviewer_name   text NOT NULL,
   reviewer_email  text NOT NULL,
   response        text,
-  helpfulness     integer NOT NULL
+  helpfulness     integer NOT NULL DEFAULT 0
 );
 
 CREATE TABLE reviews_photos (
-  id            integer NOT NULL PRIMARY KEY,
+  id            serial NOT NULL PRIMARY KEY,
   review_id     integer REFERENCES reviews(id) NOT NULL,
-  url           text NOT NULL
+  url           text NOT NULL DEFAULT '{}'
 );
 
 CREATE TABLE characteristics (
-  id            integer PRIMARY KEY NOT NULL,
+  id            serial PRIMARY KEY NOT NULL,
   product_id    integer REFERENCES reviews NOT NULL,
   name          text
 );
 
 CREATE TABLE characteristic_reviews (
-  id                    integer PRIMARY KEY NOT NULL,
+  id                    serial PRIMARY KEY NOT NULL,
   characteristics_id    integer REFERENCES characteristics(id),
   review_id             integer REFERENCES reviews(id),
   value                 integer NOT NULL
 );
 
+-- SEED ALL TABLES WITH DATA
 COPY reviews FROM '/Users/gianlazaro/Desktop/RatingsAndReviews/reviews.csv' csv header;
 COPY characteristics FROM '/Users/gianlazaro/Desktop/RatingsAndReviews/characteristics.csv' csv header;
 COPY characteristic_reviews FROM '/Users/gianlazaro/Desktop/RatingsAndReviews/characteristic_reviews.csv' csv header;
 COPY reviews_photos FROM '/Users/gianlazaro/Desktop/RatingsAndReviews/reviews_photos.csv' csv header;
+
+-- RE SYNC SEQUENCER
+SELECT setval(pg_get_serial_sequence('characteristics', 'id'), coalesce(max(id)+1, 1), false) FROM characteristics;
+SELECT setval(pg_get_serial_sequence('characteristic_reviews', 'id'), coalesce(max(id)+1, 1), false) FROM characteristic_reviews;
+SELECT setval(pg_get_serial_sequence('reviews_photos', 'id'), coalesce(max(id)+1, 1), false) FROM reviews_photos;
+SELECT setval(pg_get_serial_sequence('reviews', 'id'), coalesce(max(id)+1, 1), false) FROM reviews;
--- server/index.js
+++ server/index.js
@@ -9,59 +9,80 @@ app.use(express.json());
 app.get('/reviews', (req, res) => {
   const { page, count, sort, product_id } = req.query;
 
+  const sorter = function(srt) {
+    if(srt === 'helpful') {
+      return 'r.helpfulness'
+    } else if(srt === 'newest') {
+      return 'r.date'
+    } else {
+      return 'r.helpfulness'
+    }
+  }
+
+  // makes it so it doesn't populate data on page ZERO
+  const offset = (page * count) - count;
+
   pool.query(`
-  SELECT
-    json_build_object(
-      'product', $1::integer,
-      'page', $2::integer,
-      'count', $3::integer,
-      'results', json_agg(json_build_object(
-        'review_id', r.id,
-        'rating', r.rating,
-        'summary', r.summary,
-        'recommend', r.recommend,
-        'response', r.response,
-        'body', r.body,
-        'date', r.date,
-        'reviewer_name', r.reviewer_name,
-        'helpfulness', r.helpfulness,
-        'photos', json_build_object(
-          'url', rp.url,
-          'id', rp.id
-        )
-      ))
-      )
-  FROM reviews r INNER JOIN reviews_photos rp ON rp.review_id = r.id WHERE product_id = $1`, [product_id, page, count], (err, results) => {
+  SELECT r.id review_id, r.rating, r.summary, r.recommend, r.response, r.body, r.date, r.reviewer_name, r.helpfulness, json_agg(rp) photos FROM reviews r LEFT OUTER JOIN reviews_photos rp ON rp.review_id = r.id WHERE r.product_id = $2 AND r.reported = FALSE GROUP BY r.id, rp.* ORDER BY ${sorter(sort)} DESC LIMIT $1 OFFSET $3
+  `, [count, product_id, offset], (err, results) => {
     if (err) throw err;
-    res.send(results.rows[0]['json_build_object']);
+    res.send(results.rows);
   });
 })
+
 app.get('/reviews/meta', (req, res) => {
   const { product_id } = req.query;
   pool.query(`
-  SELECT json_agg(obj)
-  FROM (SELECT json_build_object(reviews.rating, COUNT(*)) AS obj
-            FROM reviews
-            WHERE reviews.product_id = $1
-            GROUP BY reviews.rating
-            ) AS X;
+  SELECT r.product_id, ( SELECT json_object_agg(objthree, objtwo) FROM ( SELECT reviews.rating AS objthree, COUNT(*) AS objtwo FROM reviews WHERE reviews.product_id = $1 GROUP BY reviews.rating) AS objone ) ratings, ( SELECT json_object_agg(objtwo, objone) FROM ( SELECT r.recommend AS objtwo, COUNT (*) AS objone FROM reviews r WHERE r.product_id = $1 GROUP BY r.recommend ) AS objthree ) recommended, ( SELECT json_object_agg(c.name, json_build_object('value', cr.value,'id', cr.id)) FROM characteristic_reviews cr FULL OUTER JOIN characteristics c ON cr.id = c.id WHERE c.product_id = $1 ) characteristics FROM reviews r WHERE r.product_id = $1
   `, [product_id], (err, results) => {
-    res.send(results.rows[0].json_agg);
+    res.send(results.rows[0]);
   })
 })
-app.post('/reviews', (req, res) => {
-  const { product_id, rating, summary, body, recommend, name, email, photos, characteristics } = req.body;
-  res.send(product_id);
-})
+
+const date = new Date();
+
+// !FIX ARRAYS IN PHOTOS
+app.post('/reviews', async (req, res) => {
+  const { product_id, rating, summary, body, recommend, name, email, photos, characteristics
+} = req.body;
+  const review_id = await pool.query(`
+    INSERT INTO reviews (product_id, rating, date, summary, body, recommend, reviewer_name, reviewer_email) VALUES ($1::integer, $2::integer, $3::text, $4, $5, $6, $7, $8) RETURNING id;
+  `, [product_id, rating, +date, summary, body, recommend, name, email]);
+
+  if (photos.length !== 0) {
+    pool.query(`INSERT INTO reviews_photos (review_id, url) VALUES ($1::integer, $2::text)
+    `, [review_id.rows[0].id, photos]);
+  }
+
+  if (Object.keys(characteristics).length > 0) {
+    for (const char_key in characteristics) {
+      pool.query(`
+        INSERT INTO characteristic_reviews (characteristics_id, review_id, value) VALUES ($1::integer, $2::integer, $3::integer)
+      `, [char_key, review_id.rows[0].id, characteristics[char_key]]);
+    }
+  }
+  res.send('yusssss');
+});
+
 app.put('/reviews/:review_id/helpful', (req, res) => {
   const { review_id } = req.params;
-  res.send(review_id)
+  pool.query(`
+    UPDATE reviews
+    SET helpfulness = helpfulness + 1
+    WHERE id = $1
+  `, [review_id]);
+  res.sendStatus(200);
 })
 app.put('/reviews/:review_id/report', (req, res) => {
   const { review_id } = req.params;
-  res.send(review_id)
+  pool.query(`
+  UPDATE reviews
+  SET reported = TRUE
+  WHERE id = $1
+`, [review_id]);
+  res.sendStatus(200);
 })
 
 app.listen(PORT, () => {
-  console.log(`server listening on PORT: ${PORT}`)
+  console.log(`server listening on PORT: ${PORT}`);
 });
