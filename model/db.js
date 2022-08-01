da9522984c43dfcaa8fa9d711af7af03c9e3b92d
Gian Franco Lazaro <gianlazaro7411@gmail.com>
2022-08-01 15:43:01 -0700

    essentials

---
+++ model/db.js
@@ -0,0 +1,11 @@
+const { Pool } = require('pg');
+
+const pool = new Pool({
+  host: process.env.PGHOST,
+  user: process.env.PGUSER,
+  port: process.env.PGPORT,
+  password: process.env.PGPASSWORD,
+  database: process.env.PGDATABASE,
+});
+
+module.exports = pool;
